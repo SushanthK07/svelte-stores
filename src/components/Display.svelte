374b8059ff5280523c9d9d6fc3de3090272a9a67
Sushanth Kummitha <sushanth.kummitha@needl.ai>
2024-03-03 00:56:33 +0530

    shorthand syntax for sub and unsub

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -1,19 +1,7 @@
 <script>
-  import { onDestroy } from "svelte";
   import { state } from "../store/stores.js";
-
-  let count;
-
-  const unsubscribe = state.subscribe((value) => {
-    // console.log(value);
-    count = value.count;
-  });
-
-  onDestroy(() => {
-    unsubscribe();
-  });
 </script>
 
 <main>
-  <h3>Count: {count}</h3>
+  <h3>Count: {$state.count}</h3>
 </main>
